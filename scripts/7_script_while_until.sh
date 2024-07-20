4cf741673de6177e84105df573ff11c7aee1ebf9
Brian Chaparro Cetina <bchaparro@unal.edu.co>
2024-07-20 18:30:48 -0500

    Incorporate while and until loops

---
+++ scripts/7_script_while_until.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+declare -i x=4
+
+printf "WHILING\n"
+
+while [[ $x -lt 10 ]]
+do
+    echo "number:$x"
+    (( x++ ))
+done
+
+x=4
+
+printf "UNTILING\n"
+
+until [[ $x > $x ]]
+do
+    echo "number:$x"
+    (( x++ ))
+done
